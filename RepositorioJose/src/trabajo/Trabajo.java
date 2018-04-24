6cec363fbeaa754a083086d02e78f94de0718ce3
daw <daw@AFEQUIPO15>
2018-04-24 11:35:43 +0200

    Conflicto Carlos

---
+++ RepositorioJose/src/trabajo/Trabajo.java
@@ -0,0 +1,13 @@
+package trabajo;
+
+public class Trabajo {
+
+	public static void main(String[] args) {
+		System.out.println("Trabajo Carlos y Jose");
+		
+		System.out.println("Commit por parte de Carlos");
+
+		System.out.println("Commit hecho por Carlos para crear un conflicto");
+	}
+
+}
