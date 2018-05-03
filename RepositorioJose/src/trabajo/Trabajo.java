2165b4121c509dfb97506f3324bb08e55052b5b4
josei1607c <37830155+josei1607c@users.noreply.github.com>
2018-05-03 11:22:11 +0200

    Update Trabajo.java

--- RepositorioJose/src/trabajo/Trabajo.java
+++ RepositorioJose/src/trabajo/Trabajo.java
@@ -6,14 +6,8 @@ public class Trabajo {
 		System.out.println("Trabajo Carlos y Jose");
 		
 		System.out.println("Commit por parte de Carlos");
-
-<<<<<<< HEAD
 		System.out.println("Commit hecho por Carlos para crear un conflicto");
 		System.out.println("Carlos conflicto con Jose");
-		System.out.println("Conflicto X");
-=======
-		System.out.println("Commit Jose para crear un conflicto");
->>>>>>> branch 'master' of https://github.com/josei1607c/RepositorioJose.git
 	}
 
 }
